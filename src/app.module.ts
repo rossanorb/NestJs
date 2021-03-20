d5bad588c2cdcba59d93f33f12ed55ca677faccb
Rossano Bavaresco <peo_rbavaresco@uolinc.com>
2021-03-19 22:51:31 -0300

    Done the rearranged of modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './models/database.entities';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [ProductModule],
-    exports: [ProductModule]    
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: process.env.TYPEORM_CONNECTION as any,
+            host: process.env.TYPEORM_HOST,
+            port: parseInt(process.env.TYPEORM_PORT),
+            username: process.env.TYPEORM_USERNAME,
+            password: process.env.TYPEORM_PASSWORD,
+            database: process.env.TYPEORM_DATABASE,
+            entities: entities,
+            synchronize: true,
+          }),
+        ProductModule
+    ],
+    exports: [ProductModule]
 })
 export class AppModule { }
