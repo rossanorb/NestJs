30b6d7a4ed3998ace8ac272107dcb399a2843bb2
Rossano Bavaresco <peo_rbavaresco@uolinc.com>
2021-04-25 21:18:23 -0300

    testing

--- src/products/product.service.spec.ts
+++ src/products/product.service.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
 import { getRepository, Repository } from 'typeorm';
-import { Product } from '../models/product';
+import { Product, ProductRepositoryFake } from '../models/product';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('ProductService', () => {
     let service: ProductService;
@@ -10,23 +11,40 @@ describe('ProductService', () => {
 
     beforeEach(async () => {
 
-        const mockRepository = jest.fn();
+        const mockRepository = {
+            findOne: jest.fn().mockReturnValue({}),
+            findAll: jest.fn().mockReturnValue({}),
+        };        
 
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 ProductService,
-                {
+                { 
                     provide: getRepositoryToken(Product),
-                    useClass: mockRepository
+                    useFactory: () => mockRepository
                 }
+
             ],
         }).compile();
 
-        productRepository = module.get(getRepositoryToken(Product));
         service = module.get<ProductService>(ProductService);
+        productRepository = module.get(getRepositoryToken(Product));
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
+
+    describe('finding a Product', () => {
+
+        it('returns not found when a product doesnt exist', async() => {
+            await expect(
+                service.find(1)
+            ).rejects.toThrow(new NotFoundException(`Product not found`));
+
+        });
+
+    });
+
+
 });
